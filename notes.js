1ca7b06e31241d8be9616eed77a8f7bf50f9d012
Tarun-kumsr-singh <tarunks2512@gmail.com>
2019-07-22 16:15:43 +0530

    seccond commit

--- notes.js
+++ notes.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-
+const chalk = require('chalk');
 
 const getnote = function (){
   console.log('getting the notes... ')
@@ -16,7 +16,7 @@ const addnote = function(title,body){
    }
    })
    if (duplicate.length > 0) {
-     console.log('title is taken!')
+     console.log(chalk.blue.inverse('title is taken!'))
      return false;
    }
 
@@ -42,5 +42,51 @@ try {
 }
 }
 
-addnote('t1','b1')
+// addnote('t2','b1')
+
+const removenote = function(title){
+    notes = loadnote()
+    newnotes = notes.filter((element) => {
+      return element.title != title
+    })
+    if (notes.length > newnotes.length) {
+      console.log(chalk.green.inverse('Note removed'))
+      savenote(newnotes)
+    }
+    else {
+      console.log(chalk.red.inverse('Note is not available!'));
+    }
+ }
+// removenote('t2')
 // getnote()
+
+const listnotes = () => {
+  console.log(chalk.inverse('Listing notes...'))
+  notes = loadnote()
+  if (notes.length === 0) {
+    console.log(chalk.red.inverse("No note is available!"));
+  }
+  notes.filter((element)=>{
+    console.log(element.title)
+  })
+}
+
+// listnotes()
+
+const readnote = (title) => {
+
+    const notes = loadnote()
+    const note = notes.find((note) =>  note.title === title )
+
+    console.log('Reading note...')
+
+    if (note) {
+      console.log(`Title : ${chalk.green.inverse(note.title)}`)
+      console.log(note.body)
+    }
+    else {
+      console.log(chalk.red.inverse('Note not found for the given title!'))
+    }
+}
+
+readnote("t")
